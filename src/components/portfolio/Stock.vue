745f2c1724090a84eb54ecfff1ef5d12509361bb
tr1503 <hanlinye6@gmail.com>
2019-05-26 01:58:02 -0400

    Create StockTrader

---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="col-sm-6 col-md-5">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    {{ stock.name }}
+                    <small>(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input type="number" class="form-control" placeholder="Quantity" v-model.number="quantity" :class="{danger: insufficientQuantity}">
+                </div>
+                <div class="pull-right">
+                    <button class="btn btn-success" @click="sellStock" :disabled="insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity)">{{ insufficientQuantity ? 'Not enough Stocks' : 'Sell'}}</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {mapActions} from 'vuex';
+
+    export default {
+        props: ['stock'],
+        data() {
+            return {
+                quantity: 0
+            }
+        },
+        computed: {
+            insufficientQuantity() {
+                return this.quantity > this.stock.quantity;
+            }
+        },
+        methods: {
+            ...mapActions({
+                placeSellOrders: 'sellStock'
+            }),
+            sellStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                this.placeSellOrders(order);
+                this.quantity = 0;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
